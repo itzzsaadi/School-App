e9fcc279226473e5eb1c9883aaf1223dcfd29746
itzzsaadi <saadnaseer146@gmail.com>
2026-02-22 16:25:14 +0500

    UseHttpMethodOverride added in Program.cs to Fix Update Student Action that was not working!

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -31,6 +31,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+// HTTP Method Override middleware ko add karo
+app.UseHttpMethodOverride();
 
 app.UseAuthorization();
 
