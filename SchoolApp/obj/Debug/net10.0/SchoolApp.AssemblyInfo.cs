187b7f7a07b2132be3dbf0f1f251445e25259c38
itzzsaadi <saadnaseer146@gmail.com>
2026-02-22 13:07:59 +0500

    Environment Variables Updated

--- SchoolApp/obj/Debug/net10.0/SchoolApp.AssemblyInfo.cs
+++ SchoolApp/obj/Debug/net10.0/SchoolApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789d26e85726d139f1dca1490289633f7e2fdcc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbe25d7a016346a4e997558e6cfae6eb2dea32d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
